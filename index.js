f77cd4d66086865ead58f7b504c78eca3ba8bb77
Chrikelin <Christer.resin@gmail.com>
2022-03-10 13:18:41 +0100

    add pickWord func and words list

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import feedback from './feedback.js';
+import pickWord from './pickWord.js';
+import { words } from './words.js';
 
-console.log(feedback('hallå', 'cykla'));
+// console.log(feedback('hallå', 'cykla'));
 
 // console.log(feedback('hallå', 'hallå'));
 
 // console.log(feedback('kärna', 'ändra'));
 
 // console.log(feedback('asd', 'asdf'));
+
+pickWord(words, 4, false);
---
+++ pickWord.js
@@ -0,0 +1,7 @@
+function pickWord(wordArr, wordLength, norepeat) {
+  console.log(wordArr);
+  console.log(wordLength);
+  console.log(norepeat);
+}
+
+export default pickWord;
