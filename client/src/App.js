ae48900e600238f013f8e45ae58596762508288f
Chrikelin <Christer.resin@gmail.com>
2022-03-21 13:33:07 +0100

    connect backend and frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api', { method: 'get' })
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{!data ? 'loading...' : data}</p>
       </header>
     </div>
   );
