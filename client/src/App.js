87cd9c8ff6f5ae992f0ab2db1987c8bffa532866
Chrikelin <Christer.resin@gmail.com>
2022-03-21 14:33:00 +0100

    add basic logic and styling to display status for letters on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,34 @@ import './App.css';
 /*
   - guess input
   - letter holders
+  - loop over feedback obj and set classNames based on result
   - indicators for status (correct, incorrect, misplaced) colored letters?
-  - winner? name input
+  - check winner, useState check array for all correct
+  - winner? name input and POST
 */
 
 function App() {
-  const [data, setData] = useState(null);
+  const [guess, setGuess] = useState([]);
 
   useEffect(() => {
-    fetch('/api/to-2-false')
+    fetch('/api/mount-5-false')
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setGuess(data.message));
   }, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>{!data ? 'loading...' : console.log(data)}</p>
+        <ul>
+          {guess.map((obj, index) => {
+            return (
+              <li className={obj.result} key={obj.letter + index}>
+                {obj.letter.toUpperCase()}
+              </li>
+            );
+          })}
+        </ul>
+        <p>{!guess ? 'loading...' : console.log(guess)}</p>
       </header>
     </div>
   );
