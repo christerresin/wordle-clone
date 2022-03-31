e6332bdf50827d0cfcb9a95a56d65dcdd73c1887
ChristerResin <christer.resin@gmail.com>
2022-03-31 19:05:46 +0200

    redesign of toggleswitch

--- client/src/components/ToggleSwitch.js
+++ client/src/components/ToggleSwitch.js
@@ -6,21 +6,23 @@ function ToggleSwitch(props) {
   };
 
   return (
-    <div className='app__toggle-container'>
+    <>
       {props.label}{' '}
-      <div className='app__toggle-switch' onChange={handleToggleChange}>
-        <input
-          type='checkbox'
-          className='app__toggle-checkbox'
-          name={props.label}
-          id={props.label}
-        />
-        <label className='app__toggle-label' htmlFor={props.label}>
-          <span className='inner' />
-          <span className='switch' />
-        </label>
+      <div className='app__toggle-container'>
+        <div className='app__toggle-switch' onChange={handleToggleChange}>
+          <input
+            type='checkbox'
+            className='app__toggle-checkbox'
+            name={props.label}
+            id={props.label}
+          />
+          <label className='app__toggle-label' htmlFor={props.label}>
+            <span className='inner' />
+            <span className='switch' />
+          </label>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
